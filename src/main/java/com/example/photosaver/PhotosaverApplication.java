162fdd3eb052c2244d50c65696f080e5e25d7522
l1can13 <a.a.soroka05@gmail.com>
2023-01-07 22:06:40 +0400

    init commit

---
+++ src/main/java/com/example/photosaver/PhotosaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.photosaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotosaverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/photosaver/PhotosaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photosaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotosaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
